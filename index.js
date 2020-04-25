1cb00bc77d05ab2eb02a64bdbc940f8b69eb81dd
Rohit Garg <rohit08133@gmail.com>
2020-04-25 12:20:17 +0530

    perf: A code change that improves performance

--- index.js
+++ index.js
@@ -24,3 +24,5 @@ docs: Documentation only changes
 style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
 
 refactor: A code change that neither fixes a bug nor adds a feature
+
+perf: A code change that improves performance
